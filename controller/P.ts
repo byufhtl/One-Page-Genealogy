81981cb7b8df91a0a2b122be7563fd9241a8867d
Ken <krrgithub@gmail.com>
2015-03-07 14:54:38 -0700

    I don't know.

--- controller/P.ts
+++ controller/P.ts
@@ -1,14 +1,21 @@
 ///<reference path="IStyler.ts"/>
 ///<reference path="IControllerListener.ts"/>
+///<reference path="../model/ITreeListener.ts"/>
 /**
  * Created by krr428 on 3/7/15.
  */
 
-class P implements IControllerListener {
+class P implements IControllerListener, ITreeListener {
 
     stylingPipeline: IStyler[]; // This changes based on
 
     handle(param: any): void {
         console.log("The controller listener was fired.");
     }
+
+    handleUpdate(T: ITree): void {
+        console.log("The tree was updated.");
+    }
+
+
 }
--- model/ITree.ts
+++ model/ITree.ts
@@ -1,13 +1,15 @@
 ///<reference path="../ISourceListener.ts"/>
 ///<reference path="ITreeListener.ts"/>
+///<reference path="../controller/BoxMap.ts"/>
 /**
  * Created by curtis on 3/7/15.
  */
 
 
 interface ITree {
-    getSourceListener(): ISourceListener;
+    getSourceListener(): ISourceListener;  // The tree listens for changes in the source.
     getId(id: string): INode;
     getRoot(): INode;
-    setListener(listener: ITreeListener): void;
+    setListener(listener: ITreeListener): void;  // The tree fires this when it is changed.
+    asBoxMap(): BoxMap;  // Converts itself to a box map.
 }
