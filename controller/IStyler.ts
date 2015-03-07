7f57bda5f4239b0c061a08252cf8cb765465f144
Ken <krrgithub@gmail.com>
2015-03-07 14:50:07 -0700

    Updated IStyler and P.

--- controller/IStyler.ts
+++ controller/IStyler.ts
@@ -1,7 +1,8 @@
+///<reference path="../model/IBox.ts"/>
 /**
  * Created by krr428 on 3/7/15.
  */
 
 interface IStyler {
-
+    applyStyle(boxes: IBox[]): void;
 }
--- controller/P.ts
+++ controller/P.ts
@@ -1,11 +1,14 @@
 ///<reference path="IStyler.ts"/>
+///<reference path="IControllerListener.ts"/>
 /**
  * Created by krr428 on 3/7/15.
  */
 
-class P {
+class P implements IControllerListener {
 
     stylingPipeline: IStyler[]; // This changes based on
 
-
+    handle(param: any): void {
+        console.log("The controller listener was fired.");
+    }
 }
