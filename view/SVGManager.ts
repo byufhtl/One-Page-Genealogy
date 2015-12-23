f1dedc6e4673ba9996b0ebf9a7bf3b431b959afb
Jared Crystal <jareditesrock@gmail.com>
2015-12-23 14:45:15 -0700

    zoom limits and typo fix

--- view/SVGManager.ts
+++ view/SVGManager.ts
@@ -263,18 +263,21 @@ class SVGManager implements IViewManager {
         this.refresh(this.lastBoxes);
     }
     setScale(s: number, pt:Point): void {
+        console.log("scale:",this.scale);
+        console.log("s:", s);
+        if((this.scale > .05 || s > 1) && (this.scale < 20 || s < 1)) {
+            var viewBefore:Point = this.worldToView(pt);
+            this.scale *= s;
+            var worldAfter:Point = this.viewToWorld(viewBefore);
 
-        var viewBefore: Point = this.worldToView(pt);
-        this.scale *= s;
-        var worldAfter: Point = this.viewToWorld(viewBefore);
+            var dx = worldAfter.getX() - pt.getX();
+            var dy = worldAfter.getY() - pt.getY();
 
-        var dx = worldAfter.getX() - pt.getX();
-        var dy = worldAfter.getY() - pt.getY();
+            this.translationX -= dx;
+            this.translationY -= dy;
 
-        this.translationX -= dx;
-        this.translationY -= dy;
-
-        this.refresh(this.lastBoxes);
+            this.refresh(this.lastBoxes);
+        }
     }
     setSize(width: number, height: number): void {
         this.width = width;
