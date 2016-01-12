ff48d48457722e5135da87c070d0f5523430cb02
Justin Rasband <razzleband@gmail.com>
2016-01-12 09:34:00 -0700

    moved gedcom js files to js folder.

---
+++ js/gedcomHandler.js
@@ -0,0 +1,278 @@
+/**
+ * Created by justinrasband on 8/13/15.
+ */
+var c = null;
+var gedcom = new GEDCOM()
+var indiMap = {};
+
+
+$(document).ready(function() {
+    $("#myInput").change(inputChange)
+});
+
+function inputChange(data) {
+
+    //console.log(data.target.files[0])
+    var gedInput = data.target.files[0];
+    var reader = new FileReader();
+    var gedString = "";
+
+    function readFile(e){
+
+        gedString = reader.result
+        if (gedString.charCodeAt(0) == 65279) gedString = gedString.substring(1);
+        var output = gedcom.parse(gedString);
+        useData(output)
+
+    }
+
+    reader.onload = readFile
+    reader.readAsText(gedInput);
+};
+
+function useData(gedOutput){
+
+    var jReader = new JSONREADER(gedOutput)
+    indiMap = null
+    indiMap = jReader.getIndividuals()
+
+    $('#gedcomModal').on('show.bs.modal',setOptions);
+
+    function setOptions(e){
+
+        var gedSelect = $('#gedcomSelect')
+
+        //make alphabetized array of individuals for display
+        var indiArray = []
+        for(var key in indiMap) {
+            if(key != "latestIndi" && key != "oldestIndi"){
+                if(indiMap[key].hasOwnProperty("dscBranchIds") || indiMap[key].hasOwnProperty("ascBranchIds")){
+                    indiArray.push(indiMap[key])
+                }
+            }
+        }
+
+        indiArray.sort(function (a, b) {
+
+            var aFirst = getFirstName(a);
+            var aLast = getLastName(a);
+            var bFirst = getFirstName(b);
+            var bLast = getLastName(b);
+
+            if(a.pid == indiMap["oldestIndi"] || a.pid == indiMap["latestIndi"]){
+                return -1
+            }else if(b.pid == indiMap["oldestIndi"] || b.pid == indiMap["latestIndi"]){
+                return 1
+            }
+
+            if(aLast > bLast){
+                return 1;
+            }else if(aLast < bLast){
+                return -1;
+            }else if(aFirst > bFirst){
+                return 1;
+            }else if(aFirst < bFirst){
+                return -1;
+            }
+            return 0;
+
+        });
+
+
+        //add each individual as an option to gedcomSelect element
+        for(var k = 0; k < indiArray.length;k++){
+            var key = indiArray[k].pid
+
+            if(indiMap[key].hasOwnProperty('pid')) {
+
+                var selectorString = String(key)
+                var optionString = "";
+                optionString = getDisplayName(indiMap[key].name)
+                optionString = optionString + selectorString
+
+                if(key == indiMap["latestIndi"]){
+                    optionString = optionString + " MOST RECENT"
+                }else if(key == indiMap["oldestIndi"]){
+                    optionString = optionString + " OLDEST"
+                }
+
+                var optionNode = $("<option></option>").text(optionString).html();
+
+                if(key == indiMap["latestIndi"]){
+                    $('#gedcomSelect').append($('<option>', {
+                        value: selectorString,
+                        text: optionNode,
+                        selected:true
+                    }));
+                }else{
+                    $('#gedcomSelect').append($('<option>', {
+                        value: selectorString,
+                        text: optionNode
+                    }));
+                }
+
+            }
+        }
+
+        //add 20 generations options to generationsSelect
+        for(var j = 1; j <=20;j++ ){
+            if(j == 8){
+                $('#generationsSelect').append($('<option>', {
+                    value: j,
+                    text: j,
+                    selected: true
+                }));
+            }else{
+                $('#generationsSelect').append($('<option>', {
+                    value: j,
+                    text: j
+                }));
+            }
+
+        }
+
+        function getLastName(a){
+
+            var aLast = "";
+            if(a.hasOwnProperty("surname")){
+                aLast = a.surname.toLowerCase();
+
+            }else if (a.hasOwnProperty("name")){
+                var fullname = a.name
+                var nameSplit = fullname.split(" ")
+                aLast = nameSplit[(nameSplit.length-1)].toLowerCase();
+
+            }else{
+                alast = "{"
+
+            }
+
+            var finalname = "";
+            //takes out the '/' symbol used in gedcoms to designate last name
+            for(var j = 0; j < aLast.length;j++){
+                if(aLast[j] != "/"){
+                    finalname = finalname + aLast[j]
+                }
+            }
+            aLast = finalname
+
+            if(aLast.length == 0){
+                aLast = "{"
+            }
+
+            return aLast
+        }
+
+        function getFirstName(a){
+
+            var aFirst = "";
+            if(a.hasOwnProperty("given")){
+                aFirst = a.given.toLowerCase();
+
+            }else if (a.hasOwnProperty("name")){
+                var fullname = a.name
+                var nameSplit = fullname.split(" ")
+                aFirst = nameSplit[0].toLowerCase();
+
+            }else{
+                aFirst = "{"
+
+            }
+
+            if(aFirst.length == 0){
+                aFirst = "{"
+            }
+
+            return aFirst
+        }
+
+
+        function getDisplayName(name){
+
+            var toReturn = "";
+            var splitName = name.split("/")
+            var lastName = ""
+            var firstName = ""
+
+            if(splitName.length >1){
+                lastName = splitName[(splitName.length-2)]
+            }else{
+                splitName = name.split(" ")
+                lastName = splitName[(splitName.length-1)]
+            }
+
+            firstName = splitName[0]
+            splitName = firstName.split(" ")
+
+            if(splitName.length > 5){
+                firstName = splitName[0] + " " + splitName[1]
+            }
+
+            return lastName + ", " + firstName
+        }
+
+    }//end setOptionsfunction
+
+
+    $('#gedcomModal').modal({
+        backdrop: 'static',
+        keyboard: false
+    })
+
+    $('#gedcomModal').show()
+
+    $('#gedcomSave').click(function(){
+
+        $('#gedcomModal').hide();
+
+        var dscOrAsc = $('input[name=ascOrDsc]:checked').val();
+        var rootId = $("option:selected", ('#gedcomSelect'))[0].value;
+        var generations = $("option:selected", ('#generationsSelect'))[0].value;
+
+        var chartSVGElement = cloneRemove(document.getElementById("opg-chart"))
+        while (chartSVGElement.lastChild) {
+            chartSVGElement.removeChild(chartSVGElement.lastChild);
+        }
+
+        c = new C({
+            rootId: rootId,
+            gedData: indiMap,
+            generations: generations,
+            dscOrAsc:dscOrAsc
+
+        });
+
+        var gedcomSelectElement = cloneRemove(document.getElementById('gedcomSelect'))
+        while (gedcomSelectElement.lastChild) {
+            gedcomSelectElement.removeChild(gedcomSelectElement.lastChild);
+        }
+
+        var genSelectElement = cloneRemove(document.getElementById("generationsSelect"))
+        while (genSelectElement.lastChild) {
+            genSelectElement.removeChild(genSelectElement.lastChild);
+        }
+
+
+        document.getElementById("generationsRadio").checked = true;
+
+        //cloneRemove(document.getElementById("generationsRadio"))
+        cloneRemove(document.getElementById("gedcomSave"))
+        cloneRemove(document.getElementById("gedcomModal"))
+        cloneRemove(document.getElementById("myInput"))
+
+
+        $("#myInput").change(inputChange)
+
+        //removes listeners of an element via cloning
+        function cloneRemove(element){
+            var new_element = element.cloneNode(true);
+            element.parentNode.replaceChild(new_element,element)
+            return new_element
+        }
+
+    })//end gedcomSave .click function
+
+
+
+
+}
---
+++ js/jsonReader.js
@@ -0,0 +1,365 @@
+function JSONREADER(gedData){
+    var indiMap = {};
+    var famMap = {};
+    var youngestDate = 0;;
+    var youngestIndi = {};
+    var oldestDate = 2015;
+    var oldestIndi = {};
+
+    function getYoungestIndividual(){
+        return youngestIndi
+    }
+    this.getYoungestIndividual = getYoungestIndividual
+
+    function getIndividuals(){
+
+        //console.log(gedData)
+        readJSON();
+        makeDescendantsAndAscendants()
+
+        indiMap.latestIndi = youngestIndi
+        indiMap.oldestIndi = oldestIndi
+
+        //console.log(indiMap)
+        return indiMap;
+    }
+    this.getIndividuals = getIndividuals;
+
+    function readJSON(){
+        for(var i = 0; i < gedData.length; i++){
+            var entry = gedData[i];
+
+            if(entry["tag"] == "INDI"){
+                readIndividual(entry)
+            } else if (entry["tag"] == "FAM"){
+                readFamily(entry)
+            }
+
+        }
+
+
+    }
+
+    function readFamily(entry){
+
+        var family = {};
+        family.id = entry["pointer"]
+        var members = entry["tree"]
+        readMembers(family,members)
+        famMap[family.id] = family;
+
+    }
+
+    function readMembers(family,members){
+
+        var children = []
+
+        for(var i = 0; i < members.length; i++){
+
+            var member = members[i]
+
+            if(member["tag"] == "HUSB"){
+                family.husband = member["data"]
+            }else if (member["tag"] == "WIFE"){
+                family.wife = member["data"]
+            }else if (member["tag"] == "CHIL"){
+                children.push(member["data"])
+            }else if (member["tag"] == "MARR"){
+                var marriageFacts = member["tree"]
+                for(var k = 0; k < marriageFacts.length;k++){
+                    var fact = marriageFacts[k];
+                    if(fact["tag"] == "DATE"){
+                        family.marriageDate = fact["data"]
+                    }else if (fact["tag"] == "PLAC"){
+                        family.marriagePlace = fact["data"]
+                    }
+                }
+            }
+
+        }
+        family.children = children
+    }
+
+    function readIndividual(entry){
+        var person = {};
+        person.pid = entry["pointer"]
+        var facts = entry["tree"]
+        readFacts(person,facts);
+        //console.log(person)
+        indiMap[person.pid] = person;
+    }
+
+    function readFacts(person, facts){
+
+        for (var i = 0; i < facts.length; i++){
+
+            var fact = facts[i];
+            switch(fact["tag"]){
+                case "NAME":
+                    person.name = fact["data"]
+                    if(fact["tree"].length!=0){
+                        var names = fact["tree"]
+                        for(var k = 0; k < names.length; k++){
+                            var aName = names[k];
+                            //console.log(aName)
+
+                            if(aName["tag"] == "SURN"){
+                                person.surname = aName["data"]
+                            }else if (aName["tag"] == "GIVN"){
+                                person.given = aName["data"]
+                            }else if (aName["tag"] == "NICK"){
+                                person.nickname = aName["data"]
+                            }else if (aName["tag"] == "NSFX"){
+                                person.suffixname = aName["data"]
+                            }else if (aName["tag"] == "NPFX"){
+                                person.prefixname = aName["data"]
+                            }
+
+                        }
+
+                    }
+                    break
+                case "SEX":
+                    person.gender = fact["data"]
+                    break
+                case "BIRT":
+                    getBirthInfo(person,fact);
+                    break
+                case "DEAT":
+                    getDeathInfo(person,fact);
+                    break
+                case "FAMS":
+                    if (person.hasOwnProperty("familySpouse")){
+                        person.familySpouse.push(fact["data"])
+                    }else{
+                        person.familySpouse = []
+                        person.familySpouse.push(fact["data"])
+                    }
+                    break
+                case "FAMC":
+                    if(person.hasOwnProperty("familyChild")){
+                        person.familyChild.push(fact["data"])
+                    }else{
+                        person.familyChild = []
+                        person.familyChild.push(fact["data"])
+                    }
+                    break
+                case "BAPL":
+                    getBaptismInfo(person,fact)
+                    break
+                case "ENDL":
+                    getEndowmentInfo(person,fact)
+                    break
+                default:
+                    break
+            }
+
+        }
+
+
+    }
+
+    function getBaptismInfo(person,fact){
+
+        var baptizeObject = {}
+        var facts = fact["tree"]
+        for (var i = 0; i < facts.length; i++) {
+
+            var aFact = facts[i];
+            //console.log(aFact)
+
+            if(aFact["tag"] == "DATE"){
+                baptizeObject.date = aFact["data"]
+            }else if (aFact["tag"] == "TEMP"){
+                baptizeObject.temple = aFact["data"]
+            }else if (aFact["tag"] == "PLAC"){
+                baptizeObject.place = aFact["data"]
+            }
+
+        }
+        person.baptized = baptizeObject
+
+
+    }
+
+    function getEndowmentInfo(person,fact){
+
+        var endowmentObject = {}
+        var facts = fact["tree"]
+        for (var i = 0; i < facts.length; i++) {
+
+            var aFact = facts[i];
+            if(aFact["tag"] == "DATE"){
+                endowmentObject.date = aFact["data"]
+            }else if (aFact["tag"] == "TEMP"){
+                endowmentObject.temple = aFact["data"]
+            }else if (aFact["tag"] == "PLAC"){
+                endowmentObject.place = aFact["data"]
+            }
+
+        }
+        person.endowment = endowmentObject
+
+
+    }
+
+    function getBirthInfo(person,fact){
+
+        for(var j = 0; j < fact.tree.length; j++){
+
+            var birthFact = fact.tree[j];
+
+            if (birthFact["tag"] == "DATE"){
+                //checkYear(birthFact["data"], person)
+                person.birthdate = birthFact["data"]
+            } else if (birthFact["tag"] == "PLAC"){
+                person.birthplace = birthFact["data"]
+            }
+
+        }
+    }
+
+    function getDeathInfo(person,fact){
+
+        for(var j = 0; j < fact.tree.length; j++){
+
+            var deathFact = fact.tree[j];
+
+            if (deathFact["tag"] == "DATE"){
+                //checkYear(deathFact["data"], person)
+                person.deathdate = deathFact["data"]
+            } else if (deathFact["tag"] == "PLAC"){
+                person.deathplace = deathFact["data"]
+            }
+
+        }
+    }
+
+
+    function checkYear(date, person){
+
+        var dateArray = date.split(" ")
+        var year = dateArray[dateArray.length-1].toString();
+        var re = new RegExp("[0-9]{4}");
+        //  if(!(year.match(re))){console.log(year)}
+        // && person.hasOwnProperty("ascBranchIds")
+        if ((year.match(re))&&(parseInt(year) > youngestDate)&& person.hasOwnProperty("ascBranchIds")){
+            youngestDate = parseInt(year);
+            youngestIndi = person.pid;
+        }else if ((year.match(re))&&(parseInt(year) < oldestDate)&& person.hasOwnProperty("dscBranchIds")){
+            oldestDate = parseInt(year);
+            oldestIndi = person.pid;
+        }
+    }
+
+    function handleMarriageFacts(person,spouse,thisFamily){
+        var factBundle = {}
+        if(thisFamily.hasOwnProperty("marriageDate")){
+            factBundle.marriageDate = thisFamily.marriageDate
+        }
+        if(thisFamily.hasOwnProperty("marriagePlace")){
+            factBundle.marriagePlace = thisFamily.marriagePlace
+        }
+        if(!person.hasOwnProperty("marriageFacts")){
+            person.marriageFacts = {}
+        }
+        //console.log(spouse)
+        indiMap[person.pid].marriageFacts[spouse] = factBundle
+    }
+
+
+    function makeDescendantsAndAscendants(){
+
+        for(var key in indiMap){
+            var person = indiMap[key]
+
+            if(person.hasOwnProperty("familySpouse")){
+                indiMap[key].spouses = [];
+                var familySpouses = person.familySpouse
+                for(var j = 0; j < familySpouses.length; j++){
+                    var family = familySpouses[j]
+                    var spouses = []
+                    var thisFamily= famMap[family]
+                    if(thisFamily.husband){
+                        if(thisFamily.husband != null){
+                            spouses.push(thisFamily.husband)
+                        }
+                    }
+                    if(thisFamily.wife){
+                        if(thisFamily.wife != null){
+                            spouses.push(thisFamily.wife)
+                        }
+                    }
+
+                    for (var k = 0; k < spouses.length; k++){
+                        if(spouses[k] != key){
+                            indiMap[key].spouses.push(spouses[k])
+                            handleMarriageFacts(person,spouses[k],thisFamily)
+                        }
+                    }
+                }
+
+            }
+
+            if(person.hasOwnProperty("familyChild")){
+
+                person.ascBranchIds = {};
+                var familyChild = person.familyChild
+
+                for(var i = 0; i < familyChild.length; i++){
+
+                    var family = familyChild[i]
+                    person.ascBranchIds[family] = []
+                    person.ascBranchIds[family].push(famMap[family].husband)
+                    person.ascBranchIds[family].push(famMap[family].wife)
+
+                }
+
+            }
+
+            if(person.hasOwnProperty("familySpouse")){
+
+                person.dscBranchIds = {};
+                var familySpouse = person.familySpouse
+                for(var i = 0; i < familySpouse.length; i++){
+
+                    var family = familySpouse[i]
+                    var children = famMap[family].children
+
+                    if(children.length != 0){
+                        person.dscBranchIds[family] = children
+                    }
+
+                }
+
+                if(person.hasOwnProperty("dscBranchIds")){
+                    if((Object.keys(person.dscBranchIds).length == 0)){
+                        delete person["dscBranchIds"]
+                    }
+
+                }
+
+            }
+
+            //var person = indiMap[key]
+            if(person.hasOwnProperty("ascBranchIds")||person.hasOwnProperty("dscBranchIds")){
+                if(person.hasOwnProperty("deathdate")){
+                    checkYear(person.deathdate,person)
+                }
+                if(person.hasOwnProperty("birthdate")){
+                    checkYear(person.birthdate,person)
+                }
+            }
+
+
+        }//end for every key in indi map
+
+    }//end make dsc/asc function
+
+
+
+
+
+
+
+}
